f177e210dfd0095b325760bb79a811f14391bc92
Ash Logan <ash@heyquark.com>
2023-03-21 23:36:01 +1100

    just_dance: Add JD2018

--- src/patcher/patches/just_dance.cpp
+++ src/patcher/patches/just_dance.cpp
@@ -31,5 +31,16 @@ void Patch_just_dance(uint32_t titleVer, uint64_t titleId, const rplinfo& rpls) 
         // OpenSSL X509_verify_cert (just in case lol)
         PatchInstruction((void*)(rpx->textAddr + 0x0253e1e4), 0x9421ffb8, 0x38600001);
         PatchInstruction((void*)(rpx->textAddr + 0x0253e1e8), 0xbe21000c, 0x4e800020);
+    } else if (titleId == 0x00050000'10210C00 && titleVer == 0) {
+        auto rpx = FindRPL(rpls, "wiiu_ua_engine_f.rpx");
+        if (!rpx) return;
+
+        DEBUG_FUNCTION_LINE("Detected JD2018 EUR v%d - patching...", titleVer);
+        // OpenSSL ssl_verify_cert_chain
+        PatchInstruction((void*)(rpx->textAddr + 0x024f8d08), 0x40820024, 0x60000000);
+        PatchInstruction((void*)(rpx->textAddr + 0x024f8d24), 0x38600000, 0x38600001);
+        // OpenSSL X509_verify_cert (just in case lol)
+        PatchInstruction((void*)(rpx->textAddr + 0x02537ca0), 0x9421ffb8, 0x38600001);
+        PatchInstruction((void*)(rpx->textAddr + 0x02537ca4), 0xbe21000c, 0x4e800020);
     }
 }
